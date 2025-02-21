5e2ee07c37043385b7131f1604f4740d993b902c
Amit Shil <amits@wix.com>
2025-02-21 09:28:17 +0200

    bug fix

--- server/quotes-fetcher.js
+++ server/quotes-fetcher.js
@@ -9,10 +9,12 @@ const url = `https://favqs.com/api/quotes`;
 
 const initQuotesFetcher = async () => {
     let pageIndex = 1;
+    let res;
     let hasMorePages = true;
     const rateLimiter = new RateLimiter(FETCH_PER_INTERVAL, RATE_LIMIT_SLIDING_WINDOW); //30 calls in 20 secand
-    while(hasMorePages) 
-      const res = await rateLimiter.limit(fetchQuotes.bind(null, pageIndex));
+    
+    while(hasMorePages) {
+      res = await rateLimiter.limit(fetchQuotes.bind(null, pageIndex));
       console.log('fetched new page', {page: res.page, numberQuotes: res.quotes.length});
       upsetQuotes(res.quotes); // not wating for inserting qoutes in case that if failed we will lose the data
                                // maybe a Q could have sloved it
